569cf313b926a9e9f7ca493742a2770740bdbe22
NicNator <nicolasbergamini057@gmail.com>
2026-01-28 21:20:40 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class Hello {
+
     public static void main(String[] args) {
         System.out.println("Hello World");//this line prints hello world and indentds to the next line.
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter user name");
+        String name=scan.nextLine();
+        System.out.println("username is " + name);
+
     }
 }
